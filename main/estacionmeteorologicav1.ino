9f08c91bf37b7f2f12e1c1a824804845c55bf100
Fernando Gualán <56632277+shadowekko@users.noreply.github.com>
2021-02-01 21:55:25 +0100

    Update estacionmeteorologicav1.ino

--- main/estacionmeteorologicav1.ino
+++ main/estacionmeteorologicav1.ino
@@ -7,6 +7,10 @@
 #include "DHT.h"
 #include "ArduinoJson.h"
 #include "Wire.h"
+#include "WiFiClient.h"
+#include "WebServer.h"
+#include "ESPmDNS.h"
+#include "Update.h"
 
 #define pinSensorAir        34
 #define pinSensorLoudness   35
@@ -29,7 +33,7 @@
 #define CHAT_ID             "512655357"
 
 
-
+const char* host = "esp32";
 
 const char* ssid = "SBC";
 const char* password = "sbc$2020";
@@ -40,8 +44,10 @@ ThingsBoard tb(cliente);
 AirQualitySensor sensor1(pinSensorAir);
 DHT sensorDHT(pinSensorDHT, DHT22);
 UniversalTelegramBot bot(BOT_TOKEN, client);
+WebServer server(80);
 
-int botRequestDelay = 2000;
+
+int botRequestDelay = 1000;
 unsigned long lastTimeBotRan;
 
 String readAirQuality() {
@@ -116,17 +122,45 @@ String readLightSensorData () {
     return message;
 }
 
+
+// UVSensor Implementacion
+float readUV(){
+  int s = 1000;
+  float v = 5.0;
+  float sum = 0;
+  for(int i=0; i<s; i++){
+    float v = analogRead(pinSensorUV);
+    sum = v + sum;
+    delay(2);
+  }
+  float sensor_value_average = sum / s;
+  float mv = sensor_value_average * v;
+
+  return mv;
+}
+
+float getIndex (float read_mV){
+  float voltage = read_mV / 1024.0;
+  int uv_index = voltage / 0.1;
+
+  return uv_index;
+}
+
+
 String readUVSensor () {
-    int uvRaw;
+  
+    float uvRaw;
+    float uvIndex;
     String message;
 
-    uvRaw = analogRead(pinSensorUV);
-    Serial.println(uvRaw);
-
-    message = "UV Value: " + String(uvRaw) + "\n";
+    uvRaw = readUV();
+    uvIndex = getIndex(uvRaw);
+    Serial.println(uvIndex);
+    
+    message = "UV Index: " + String(uvIndex) + "\n";
 
     Serial.println("Sending data...");
-    tb.sendTelemetryInt("UV value", uvRaw);
+    tb.sendTelemetryFloat("UV Index", uvIndex);
     tb.loop();
     return message;
 }
@@ -148,8 +182,8 @@ String readDHTSensor() {
     message = "Temperatura: " + String(temperature) +  "ºC / Humedad: " + String(humidity) + "% " +"\n";
 
     Serial.println("Sending data..");
-    tb.sendTelemetryInt("Temperature value", temperature);
-    tb.sendTelemetryInt("Humidity value", humidity);
+    tb.sendTelemetryFloat("Temperature value", temperature);
+    tb.sendTelemetryFloat("Humidity value", humidity);
     tb.loop(); 
     return message;
   }
@@ -200,12 +234,97 @@ void handleNewMessages (int numNewMessages)  {
           if(text == "/readings") {        
               String message = getReadings();
               bot.sendMessage(chat_id, message, "");
- 
+            
           }
       }
 
  }
 
+
+/*
+ * Login page
+ */
+const char* loginIndex = 
+ "<form name='loginForm'>"
+      "<table width='20%' bgcolor='A09F9F' align='center'>"
+          "<tr>"
+              "<td colspan=2>"
+                  "<center><font size=4><b>ESP32 Login Page</b></font></center>"
+                  "<br>"
+              "</td>"
+              "<br>"
+              "<br>"
+          "</tr>"
+          "<td>Username:</td>"
+          "<td><input type='text' size=25 name='userid'><br></td>"
+          "</tr>"
+          "<br>"
+          "<br>"
+          "<tr>"
+              "<td>Password:</td>"
+              "<td><input type='Password' size=25 name='pwd'><br></td>"
+              "<br>"
+              "<br>"
+          "</tr>"
+          "<tr>"
+              "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+          "</tr>"
+      "</table>"
+  "</form>"
+  "<script>"
+      "function check(form)"
+      "{"
+      "if(form.userid.value=='admin' && form.pwd.value=='admin')"
+      "{"
+      "window.open('/serverIndex')"
+      "}"
+      "else"
+      "{"
+      " alert('Error Password or Username')/*displays error message*/"
+      "}"
+      "}"
+"</script>";
+ 
+/*
+ * Server Index Page
+ */
+ 
+const char* serverIndex = 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+   "<input type='file' name='update'>"
+        "<input type='submit' value='Update'>"
+    "</form>"
+   "<div id='prg'>progress: 0%</div>"
+   "<script>"
+    "$('form').submit(function(e){"
+    "e.preventDefault();"
+    "var form = $('#upload_form')[0];"
+    "var data = new FormData(form);"
+    " $.ajax({"
+    "url: '/update',"
+    "type: 'POST',"
+    "data: data,"
+    "contentType: false,"
+    "processData:false,"
+    "xhr: function() {"
+    "var xhr = new window.XMLHttpRequest();"
+    "xhr.upload.addEventListener('progress', function(evt) {"
+    "if (evt.lengthComputable) {"
+    "var per = evt.loaded / evt.total;"
+    "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+    "}"
+    "}, false);"
+    "return xhr;"
+    "},"
+    "success:function(d, s) {"
+    "console.log('success!')" 
+   "},"
+   "error: function (a, b, c) {"
+   "}"
+   "});"
+   "});"
+ "</script>";
 
 
 void setup() {
@@ -219,7 +338,18 @@ void setup() {
       delay(500);
       Serial.print(".");
     }
-    Serial.println("Wifi conectado!");
+    Serial.print("Wifi conectado: ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    /*use mdns for host name resolution*/
+    if (!MDNS.begin(host)) { //http://esp32.local
+      Serial.println("Error setting up MDNS responder!");
+      while (1) {
+      delay(1000);
+     }
+    }
     
     //Led of sensor Light
     pinMode(pinSensorLight,OUTPUT);
@@ -229,12 +359,55 @@ void setup() {
 
     //Led
     pinMode (LED, OUTPUT);
+
+    //GUVA
+   
+
+    Serial.println("mDNS responder started");
+    /*return index page which is stored in serverIndex */
+      server.on("/", HTTP_GET, []() {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/html", loginIndex);
+      });
+      server.on("/serverIndex", HTTP_GET, []() {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/html", serverIndex);
+      });
+      /*handling uploading firmware file */
+      server.on("/update", HTTP_POST, []() {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+        ESP.restart();
+      }, []() {
+        HTTPUpload& upload = server.upload();
+        if (upload.status == UPLOAD_FILE_START) {
+          Serial.printf("Update: %s\n", upload.filename.c_str());
+          if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+            Update.printError(Serial);
+          }
+        } else if (upload.status == UPLOAD_FILE_WRITE) {
+          /* flashing firmware to ESP*/
+          if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+            Update.printError(Serial);
+          }
+        } else if (upload.status == UPLOAD_FILE_END) {
+          if (Update.end(true)) { //true to set the size to the current progress
+            Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+          } else {
+            Update.printError(Serial);
+          }
+        }
+      });
+      server.begin();
  
 }
 
 void loop() {
+
+    server.handleClient();
+    delay(1);
   
-    if (millis() > lastTimeBotRan + botRequestDelay)  {
+    if (millis() - lastTimeBotRan > botRequestDelay)  {
       int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
   
       while(numNewMessages) {
@@ -243,12 +416,11 @@ void loop() {
         numNewMessages = bot.getUpdates(bot.last_message_received + 1);
      }
      lastTimeBotRan = millis();
-   
     }
 
     
     
-    if (tb.connected()) {
+    if (!tb.connected()) {
       // Conectar a ThingsBoard
       Serial.print("Connecting to: ");
       Serial.print(THINGSBOARD_SERVER);
@@ -278,7 +450,8 @@ void loop() {
 
     Serial.println("----------------------------------------");
     
-    delay(5000);
+    delay(10000);
 
+    
     
 }
